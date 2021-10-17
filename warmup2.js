d13c9ca6a706c5f3d80eb7dd1cfb1bd7df8213db
Luis Lopez <llopez1518@gmail.com>
2021-10-16 22:54:52 -0600

    work on warmup2

--- warmup2.js
+++ warmup2.js
@@ -12,7 +12,6 @@ function stringTimes(str, n) {
 }
 
 stringTimes("Hi", 2);
-*/
 
 // Given a string and a non-negative int n, we'll say that the front of the
 // string is the first 3 chars, or whatever is there if the string is less than
@@ -33,3 +32,27 @@ function frontTimes(str, n) {
 }
 
 frontTimes("Chocolate", 2);
+
+// Count the number of 'xx' in the given string.
+// We'll say that overlapping is allowed, so 'xxx' contains 2 'xx'.
+
+function countXX(str) {
+  let result = 0;
+  for (let i = 0; i < str.length - 1; i++) {
+    if (str.substring(i, i + 2) === "xx") {
+      result += 1;
+    }
+  }
+  console.log(result);
+  return result;
+}
+
+countXX("abcxxxx");
+*/
+
+// Given a string, return true if the first instance of "x" 
+// in the string is immediately followed by another "x".
+
+function doubleX(str) {
+  
+}
