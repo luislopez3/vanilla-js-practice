59fed54037e692a432c702346ea5364bea5ec587
Luis Lopez <llopez1518@gmail.com>
2021-10-17 20:55:04 -0600

    cont'd work on warmup2

--- warmup2.js
+++ warmup2.js
@@ -48,11 +48,66 @@ function countXX(str) {
 }
 
 countXX("abcxxxx");
-*/
 
-// Given a string, return true if the first instance of "x" 
+// Given a string, return true if the first instance of "x"
 // in the string is immediately followed by another "x".
 
 function doubleX(str) {
-  
+  let i = str.indexOf("x");
+  const x = str.substring(i);
+  console.log(x.startsWith("xx"));
+  return x.startsWith("xx");
 }
+
+doubleX("axxbb");
+
+// Given a string, return a new string made of every other char 
+// starting with the first, so "Hello" yields "Hlo".
+
+function stringBits(str) {
+  let result = "";
+  for (let i = 0; i < str.length; i += 2) {
+    result += str.substring(i, i + 1);
+  }
+  console.log(result);
+  return result;
+}
+
+stringBits("Hello");
+
+// Given a non-empty string like "Code" return a string like "CCoCodCode".
+
+function stringSplosion(str){
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    result += str.substring(0, i + 1);
+  }
+  console.log(result);
+  return result;
+}
+
+stringSplosion("Code");
+*/
+
+// Given a string, return the count of the number of times that a substring
+// length 2 appears in the string and also as the last 2 chars of the string, so
+// "hixxxhi" yields 1 (we won't count the end substring).
+
+function last2(str) {
+  if (str.length < 2) {
+    return 0;
+  }
+  let end = str.substring(str.length - 2);
+  let count = 0;
+  for (let i = 0; i < str.length - 2; i++) {
+    let sub = str.substring(i, i + 2);
+    if (sub === end) {
+      count += 1;
+    }
+  }
+  console.log(count);
+  return count;
+}
+
+last2("hixxhi");
+
