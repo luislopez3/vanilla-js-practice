27520017f1284b8f2d0e6257ea4845a0f1972f48
Luis Lopez <llopez1518@gmail.com>
2021-10-20 22:26:12 -0600

    finish w/ warmup2 section

--- warmup2.js
+++ warmup2.js
@@ -139,7 +139,6 @@ function arrayFront9(nums) {
 }
 
 arrayFront9([1, 2, 3, 4, 9]);
-*/
 
 // Given an array of ints, return true if the sequence of numbers 1, 2, 3
 // appears in the array somewhere.
@@ -156,3 +155,137 @@ function array123(nums) {
 }
 
 array123([5, 4, 3, 1, 2, 2]);
+
+// Given 2 strings, a and b, return the number of the positions where they 
+// contain the same length 2 substring. So "xxcaazz" and "xxbaaz" yields 3,
+// since the "xx" "xx", "aa", and "az" substrings appear in the same place 
+// in both strings.
+
+function stringMatch(a, b) {
+  const len = Math.min(a.length, b.length);
+  let count = 0;
+  for (let i = 0; i < len - 1; i++) {
+    const aSub = a.substring(i, i + 2);
+    const bSub = b.substring(i, i + 2);
+    if (aSub === bSub) {
+      count += 1;
+    }
+  }
+  console.log(count);
+  return count;
+}
+
+stringMatch("xxcaazz", "xxbaaz");
+
+// Given a string, return a version where all the "x" have been removed. 
+// Except an "x" at the very start or end should not be removed.
+
+function stringX(str) {
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    if (!(i > 0 && i < str.length - 1 && str.substring(i, i + 1) === "x")) {
+      result += str.substring(i, i + 1);
+    }
+  }
+  console.log(result);
+  return result;
+}
+
+stringX("x");
+
+// Given a string, return a string made of the chars at indexes 0,1, 4,5, 8,9
+// ... so "kittens" yields "kien".
+
+function altPairs(str) {
+  let result = "";
+  for (let i = 0; i < str.length; i += 4) {
+    let end = i + 2;
+    if (end > str.length) {
+      end = str.length;
+    }
+    result += str.substring(i, end);
+  }
+  console.log(result);
+  return result;
+}
+
+altPairs("kitten");
+
+// Suppose the string "yak" is unlucky. Given a string, return a version where
+// all the "yak" are removed, but the "a" can be any char. The "yak" strings
+// will not overlap.
+
+function stringYak(str) {
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    if (
+      i + 2 < str.length &&
+      str.charAt(i) === "y" &&
+      str.charAt(i + 2) === "k"
+    ) {
+      i += 2;
+    } else {
+      result += str.charAt(i);
+    }
+  }
+  console.log(result);
+  return result;
+}
+
+stringYak("yakpak");
+
+// Given an array of ints, return the number of times that two 6's are next to 
+// each other in the array. Also count instances where the second "6" 
+// is actually a 7.
+
+function array667(nums) {
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 6) {
+      if (nums[i + 1] === 6 || nums[i + 1] === 7) {
+        count += 1;
+      }
+    }
+  }
+  console.log(count);
+  return count;
+}
+
+array667([6, 7, 2, 6]);
+
+// Given an array of ints, we'll say that a triple is a value appearing 3 times
+// in a row in the array. Return true if the array does not contain any triples.
+
+function noTriples(nums) {
+  for (let i = 0; i < nums.length - 2; i++) {
+    const first = nums[i];
+    if (nums[i + 1] === first && nums[i + 2] === first) {
+      console.log(false);
+      return false;
+    }
+  }
+  console.log(true);
+  return true;
+}
+
+noTriples([1, 1, 2, 2, 1]);
+*/
+
+// Given an array of ints, return true if it contains a 2, 7, 1 pattern -- a value, 
+// followed by the value plus 5, followed by the value minus 1. 
+// Additionally the 271 counts even if the "1" differs by 
+// 2 or less from the correct value.
+
+function has271(nums) {
+  for (let i = 0; i < nums.length - 2; i++) {
+    const first = nums[i];
+    if (nums[i + 1] === (first + 5) && Math.abs(nums[i + 2] - (first - 1)) <= 2) {
+      console.log(true);
+      return true;
+    }
+  }
+  console.log(false);
+  return false;
+}
+
+has271([1, 2, 8, 1]);
