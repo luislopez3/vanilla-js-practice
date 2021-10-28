53b5d3ee820e6f749b9579308d6a3ce8ef1c2b47
Luis Lopez <llopez1518@gmail.com>
2021-10-27 22:55:14 -0600

    work on codewars exercises

---
+++ code-wars/code-wars.js
@@ -0,0 +1,10 @@
+// Given a string of space separated numbers
+// Return the highest and lowest number.
+
+function highAndLow(numbers) {
+    let number = numbers.split(" ");
+    console.log(`${Math.max(...number)} ${Math.min(...number)}`);
+    return `${Math.max(...number)} ${Math.min(...number)}`;
+}
+
+highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4");
